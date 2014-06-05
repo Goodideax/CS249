d8cae866245210a8af38c6231261e085163bccbf
Spencer Tung <spencer.zh.tung@gmail.com>
2014-06-04 23:47:33 -0700

    Added a loader script for the four data sets

---
+++ loader.R
@@ -0,0 +1,7 @@
+output_svm_1000 <- read.csv("~/Documents/UCLA/SP14/CS249/Project/sampleData/output_svm_1000.csv")
+output_gp_1000 <- read.csv("~/Documents/UCLA/SP14/CS249/Project/sampleData/output_gp_1000.csv")
+output_gbm_1000 <- read.csv("~/Documents/UCLA/SP14/CS249/Project/sampleData/output_gbm_1000.csv")
+output_1000 <- read.csv("~/Documents/UCLA/SP14/CS249/Project/sampleData/output_1000.csv")
+
+
+
